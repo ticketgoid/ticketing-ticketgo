f386783867ed1aabeb4c3a35b960d6f1a35aca2a
TicketGo <ticketgo.ticketing.id@gmail.com>
2025-10-22 00:21:54 +0700

    Update get-events.js

--- netlify/functions/get-events.js
+++ netlify/functions/get-events.js
@@ -2,8 +2,8 @@
 const fetch = require('node-fetch');
 
 exports.handler = async function (event, context) {
-  const { AIRTABLE_API_KEY, AIRTABLE_BASE_ID } = process.env;
-  const url = `https://api.airtable.com/v0/${AIRTABLE_BASE_ID}/Events?sort%5B0%5D%5Bfield%5D=Prioritas&sort%5B0%5D%5Bdirection%5D=desc&sort%5B1%5D%5Bfield%5D=Urutan&sort%5B1%5D%5Bdirection%5D=asc&sort%5B2%5D%5Bfield%5D=Waktu&sort%5B2%5D%5Bdirection%5D=asc`;
+  const { AIRTABLE_API_KEY, AIRTABLE_BASE_ID_EVENTS } = process.env;
+  const url = `https://api.airtable.com/v0/${AIRTABLE_BASE_ID_EVENT}/Events?sort%5B0%5D%5Bfield%5D=Prioritas&sort%5B0%5D%5Bdirection%5D=desc&sort%5B1%5D%5Bfield%5D=Urutan&sort%5B1%5D%5Bdirection%5D=asc&sort%5B2%5D%5Bfield%5D=Waktu&sort%5B2%5D%5Bdirection%5D=asc`;
 
   try {
     const response = await fetch(url, {
