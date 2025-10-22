b0b7450f2fec20d67d1c273ca7d7b9c21d4a3be3
TicketGo <ticketgo.ticketing.id@gmail.com>
2025-10-22 22:09:14 +0700

    Create get-event-price.js

---
+++ netlify/functions/get-event-price.js
@@ -0,0 +1,31 @@
+// File: netlify/functions/get-events.js
+const fetch = require('node-fetch');
+
+exports.handler = async function (event, context) {
+  const { AIRTABLE_API_KEY, AIRTABLE_BASE_ID_SEAT } = process.env;
+  const url = `https://api.airtable.com/v0/${AIRTABLE_BASE_ID_SEAT}/Events?sort%5B0%5D%5Bfield%5D=Prioritas&sort%5B0%5D%5Bdirection%5D=desc&sort%5B1%5D%5Bfield%5D=Urutan&sort%5B1%5D%5Bdirection%5D=asc&sort%5B2%5D%5Bfield%5D=Waktu&sort%5B2%5D%5Bdirection%5D=asc`;
+
+  try {
+    const response = await fetch(url, {
+      headers: {
+        'Authorization': `Bearer ${AIRTABLE_API_KEY}`,
+      },
+    });
+
+    if (!response.ok) {
+      return { statusCode: response.status, body: response.statusText };
+    }
+
+    const data = await response.json();
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data),
+    };
+  } catch (error) {
+    console.error('Error fetching events from Airtable:', error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Failed to fetch events' }),
+    };
+  }
+};
