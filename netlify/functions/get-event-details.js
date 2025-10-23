723adc546843db6187469ba5b48e28921cafb728
TicketGo <ticketgo.ticketing.id@gmail.com>
2025-10-23 10:30:35 +0700

    Update get-event-details.js

--- netlify/functions/get-event-details.js
+++ netlify/functions/get-event-details.js
@@ -1,6 +1,3 @@
-// GANTI SELURUH ISI FILE DENGAN KODE BARU INI
-const fetch = require('node-fetch');
-
 // Fungsi pembantu untuk berkomunikasi dengan Airtable API
 const airtableFetch = async (apiKey, url) => {
     const response = await fetch(url, { headers: { 'Authorization': `Bearer ${apiKey}` } });
